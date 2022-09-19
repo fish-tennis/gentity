9499a1f08c65326b37b80f4a730cda4a21e8149c
fish-tennis <93663886@qq.com>
2022-09-19 17:54:16 +0800

    PlayerMgr

--- player.go
+++ player.go
@@ -1,5 +1,10 @@
 package gentity
 
+import (
+	"github.com/fish-tennis/gnet"
+	"google.golang.org/protobuf/proto"
+)
+
 type Player interface {
 	Entity
 
@@ -11,10 +16,12 @@ type Player interface {
 
 	// 区服id
 	GetRegionId() int32
+
+	Send(command gnet.PacketCommand, message proto.Message) bool
 }
 
 type PlayerMgr interface {
-	GetPlayer(playerId int64) *Player
-	AddPlayer(player *Player)
-	RemovePlayer(player *Player)
+	GetPlayer(playerId int64) Player
+	AddPlayer(player Player)
+	RemovePlayer(player Player)
 }
